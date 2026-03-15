7c64dbcee226a30f7f115360e32119b8a28a4bf6
Jpala21 <82098767+JPala21@users.noreply.github.com>
2026-03-15 10:41:52 -0500

    Transiciones  entre paginas

--- tu_fondo/lib/config/routes.dart
+++ tu_fondo/lib/config/routes.dart
@@ -1,8 +1,38 @@
 import 'package:go_router/go_router.dart';
+import 'package:page_transition/page_transition.dart';
 import 'package:tu_fondo/modules/login/pages/login.dart';
-
 
 final GoRouter router = GoRouter(
   initialLocation: '/',
-  routes: [GoRoute(path: '/', builder: (context, state) => Login())],
+  routes: [
+    GoRoute(
+      path: '/',
+      pageBuilder: (context, state) {
+        return CustomTransitionPage(
+          child: Login(),
+          transitionsBuilder: (context, animation, secondaryAnimation, child) {
+            return PageTransition(
+              type: .leftToRightPop,
+              child: child,
+            ).buildTransitions(context, animation, secondaryAnimation, child);
+          },
+        );
+      },
+    ),
+  ],
 );
+
+
+
+
+
+// fade	
+// rightToLeft	
+// leftToRight	
+// topToBottom	
+// bottomToTop	
+// scale	
+// rotate	
+// size	
+// rightToLeftWithFade	
+// leftToRightWithFade	
